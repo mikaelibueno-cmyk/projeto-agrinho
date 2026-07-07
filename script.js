e634c2d2f7aef3cc8c2ab6c300e43a3fe18edfd8
mikaelibueno-cmyk <mikaeli.bueno@escola.pr.gov.br>
2026-07-07 17:21:46 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,53 @@
-```javascript id="l5c4r8"
+```javascript id="v8w5jp"
 let pontos = 0;
 
+
 function adicionarPontos(valor){
+
     pontos += valor;
 
     document.getElementById("pontos").innerHTML = pontos;
+
 }
+
+
 
 function resultadoFinal(){
 
     let resultado = document.getElementById("resultado");
 
+
     if(pontos >= 80){
+
         resultado.innerHTML =
         "🎉 Parabéns! Você construiu uma Fazenda Sustentável!";
+
     }
+
 
     else if(pontos >= 50){
+
         resultado.innerHTML =
         "👍 Bom trabalho, mas ainda pode melhorar.";
+
     }
 
+
     else{
+
         resultado.innerHTML =
         "⚠️ Sua fazenda precisa de práticas mais sustentáveis.";
+
     }
+
 }
+
+
+
 
 function gerarCertificado(){
 
     document.getElementById("certificado").innerHTML =
     "🏅 Certificado de Agricultor Sustentável Conquistado!";
+
 }
-```
